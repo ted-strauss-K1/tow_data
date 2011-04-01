33e1f83f110bb98c816f0a4baf4af253da6379aa
test <test@5b39122e-378b-4055-87f5-8dd9c490926f>
2011-04-01 16:03:48 +0000

    template for dataset badge block

---
+++ sites/all/modules/_custom/tow/templates/tow_block-dataset_badge.tpl.php
@@ -0,0 +1,12 @@
+<?php
+// $Id: tow_block-dataset_badge.tpl.php,v 1.0.0.0 2011/02/28 23:42:15 tow Exp $
+?>
+<div class="badge">
+  <p><?php print $posted_by; ?></p>
+  <p><?php print $count; ?></p>
+  <p><?php print $searches; ?></p>
+  <p><?php print $added; ?></p>
+  <p><?php print $updated; ?></p>
+  <p><?php print $access; ?></p>
+</div>
+
