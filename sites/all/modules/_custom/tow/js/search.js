aeea6ee207052de83645ceb75ebdbefa89720a9f
test <test@5b39122e-378b-4055-87f5-8dd9c490926f>
2011-08-02 09:08:32 +0000

    Search.js: fields behaviour on the search within dataset page added; other files: minor changes.

--- sites/all/modules/_custom/tow/js/search.js
+++ sites/all/modules/_custom/tow/js/search.js
@@ -1,5 +1,16 @@
 Drupal.behaviors.Search = function(context) {
-  
+	$("#edit-tables-select option").each(function () {
+		// hide tables to be displayed
+		id = "#edit-fields-select-tables-" + $(this).val() + "-wrapper";
+		$(id).hide();
+		// hide fields to be displayed 
+		id = 'div[id*="t' + $(this).val() + '"]';
+		$(id).hide();
+		// hide constraint fields 		
+		id = 'option[value*="t' + $(this).val() + '"]';
+		$(id).hide();
+	  }); 
+	
   $('.remove-filter').click(function() {
     $(this).parent().hide();
     //$(this).parent().children('.value-field').children('div').children('input').val('4mpWbjErTjPdVwao');
@@ -11,5 +22,35 @@ Drupal.behaviors.Search = function(context) {
     $('option:selected').removeAttr('selected');
     
     $('#filter-items-wrapper').children().slice(1).hide();
+  });
+  
+  $('#edit-tables-select').change(function() {
+	  var str = "";
+	  var id = "";
+	  //alert($(this).text());
+	  
+	  $("#edit-tables-select option").each(function () {
+		// hide tables to be displayed
+		id = "#edit-fields-select-tables-" + $(this).val() + "-wrapper";
+		$(id).hide();
+		// hide fields to be displayed 
+		id = 'div[id*="t' + $(this).val() + '"]';
+		$(id).hide();
+		// hide constraint fields 		
+		id = 'option[value*="t' + $(this).val() + '"]';
+		$(id).hide();
+	  }); 
+	  $("#edit-tables-select option:selected").each(function () {
+		// show tables to be displayed
+		id = "#edit-fields-select-tables-" + $(this).val() + "-wrapper";
+		$(id).show();
+		// show fields to be displayed 
+		id = 'div[id*="t' + $(this).val() + '"]';
+		$(id).show();
+		// show constraint fields 
+		id = 'option[value*="t' + $(this).val() + '"]';
+		$(id).show();
+		
+	  });
   });
 };
