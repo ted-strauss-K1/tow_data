2ed0709aaa8827d2172b708a3d8758139b37524d
root <root@5b39122e-378b-4055-87f5-8dd9c490926f>
2012-01-20 12:25:40 +0000

    (i) Modified script for generation data to set random time of creation and changing and not to set node teaser. (ii) Added outer search (search among all datasets) at homepage.

--- sites/all/themes/themes/fusion/fusion_tow/search-result.tpl.php
+++ sites/all/themes/themes/fusion/fusion_tow/search-result.tpl.php
@@ -1,13 +1,18 @@
 <pre>
 <?php
 //if ($id == 10) 
-//  print_r(get_defined_vars());
+  //print_r(get_defined_vars());
+  //dpm($result);
 ?>
 </pre>
 
 <dt class="title">
   <a href="<?php print $url; ?>"><?php print $title; ?></a>
 </dt>
+<dt class="body">
+  <?php print $result['teaser']; ?></a>
+</dt>
+
 <dd>
   <div class="statistics">
     
@@ -31,6 +36,40 @@
         ?>
       </div>
     </div>
-  </div>
+
+    <div class="dataset-badge">
+      <div class="posted-date">
+        <?php
+          print $result['posted_date']; 
+        ?>
+      </div>
+      <div class="user-link">
+        <?php
+          print $result['user']; 
+        ?>
+      </div>
+      <div class="user-picture">
+        <?php
+          print $result['picture']; 
+        ?>
+      </div>
+    </div>
+
+    <div class="taxonomy">
+      <div class="category">
+        <?php
+          print t('category: ') . $result['category'];
+        ?>
+      </div>
+	  
+      <?php
+		print 'tags: ';
+        foreach ($result['tags'] as $term) {
+			print '<div class="tags">' . $term . '</div>'; 
+		}
+      ?>
+      
+    </div>
+</div>
 
 </dd>
