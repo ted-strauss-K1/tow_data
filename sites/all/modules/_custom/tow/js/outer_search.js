cef7ebb5fa0f09f9e081c37645fe4975d2b2ef78
root <root@5b39122e-378b-4055-87f5-8dd9c490926f>
2012-01-28 08:25:51 +0000

    (i) Added list of active filters to outer search; (ii) Added tabs block to outer search with AJAX-switching; (iii) Made number of minor corrections in tow module causing warnings; (iv) Fixed bug of calculating offacet numbers for amount of rows/searches menus

--- sites/all/modules/_custom/tow/js/outer_search.js
+++ sites/all/modules/_custom/tow/js/outer_search.js
@@ -22,17 +22,21 @@ Drupal.behaviors.outer_search = function(context) {
 	});
 //	disabling page reload on submit
 	$('#edit-submit').live('click', function(e) {
-		e.preventDefault();
-		$(this).attr('disabled','true');
-		var url = build_query();
-		do_search(url);
+		if (e.button == 0) {
+			e.preventDefault();
+			$(this).attr('disabled','true');
+			var url = build_query();
+			do_search(url);
+		}
 	});
 	
 //	disabling tag links
-	$('.apachesolr-facet, .apachesolr-unclick, .active').live('click', function(e) {
-		e.preventDefault();
-		var url = 'http://' + location.host + '/search_outer_ajax' + $(this).attr('href');
-		do_search(url);
+	$('.apachesolr-facet, .apachesolr-unclick, .active, .tow-search-outer-tab').live('click', function(e) {
+		if (e.button == 0) {
+			e.preventDefault();
+			var url = 'http://' + location.host + '/search_outer_ajax' + $(this).attr('href');
+			do_search(url);
+		}
 	});	
 	
 //	building search query from form
@@ -66,21 +70,43 @@ Drupal.behaviors.outer_search = function(context) {
 			query = '';
 		}
 		
-		return 'http://' + location.host + '/search_outer_ajax/newest' + path + '?' + query;
+		var tab = $('#edit-current-tab').val();
+		
+		return 'http://' + location.host + '/search_outer_ajax/' + tab + path + '?' + query;
 	
 	}
 	
 //	search and associated actions
 	var do_search = function (url) {
 		$('#content-content').html($('#edit-search-placeholder').val());
+		url = url.replace('#', '');
 		$.ajax({
 			url: url,
 			success: function(data) {
 				$('#content-content').html(data.results);
 				$('#tow-search-outer-filter-form').html(data.form);
-				$('#block-tow-search_outer_number_of_results').html(data.number);
+				$('#block-tow-search_outer_number_of_results > .inner > .content').html(data.number);
+				$('#block-tow-search_outer_tabs > .inner > .content').html(data.tabs);
+				
+				
+				$('.apachesolr-hidden-facet', context).hide();
+				
+				$('<a href="#" class="apachesolr-showhide"></a>').text(Drupal.t('Show more')).click(function() {
+				if ($(this).parent().find('.apachesolr-hidden-facet:visible').length == 0) {
+					$(this).parent().find('.apachesolr-hidden-facet').show();
+					$(this).text(Drupal.t('Show fewer'));
+				}
+				else {
+					$(this).parent().find('.apachesolr-hidden-facet').hide();
+					$(this).text(Drupal.t('Show more'));
+				}
+				return false;
+				}).appendTo($('.item-list:has(.apachesolr-hidden-facet)', context));	
+
+//				location.hash = 'searched';
 			},
 			dataType: 'json'
 		});	  
 	}
+	
 }
