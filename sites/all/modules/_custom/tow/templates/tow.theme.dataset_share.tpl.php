354354681a881e2aae60189f286ec667806bbcb0
root <root@tow1.(none)>
2012-10-09 03:19:20 -0500

    Moving to GIT.

---
+++ sites/all/modules/_custom/tow/templates/tow.theme.dataset_share.tpl.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @file
+ * 
+ * Template for dataset 'Share' block.
+ */
+
+?>
+<div class="a2a_kit">
+  <a class="a2a_button_facebook">
+    <img src="/sites/default/files/pictures/facebook.png" border="0" alt="Facebook" width="30" height="30"/>
+  </a>
+  <a class="a2a_button_twitter">
+    <img src="/sites/default/files/pictures/twitter.png" border="0" alt="Twitter" width="30" height="30"/>
+	</a>
+	<a class="a2a_button_google_gmail">
+    <img src="/sites/default/files/pictures/google+.png" border="0" alt="Google+" width="30" height="30"/>
+  </a>
+</div>
+<script type="text/javascript" src="http://static.addtoany.com/menu/page.js"></script>
