787e10febefb6c2635619f7e91f7e6b67451ac2a
= <pavel.da@core-tech.ru>
2013-02-11 10:06:19 +0000

    Applied multiselect and quicksearch for text facets at inner search. Added corresponded jquery libraries. Made several css changes

---
+++ sites/all/modules/_custom/tow/js/lou-multi-select/test/spec/SpecHelper.js
@@ -0,0 +1,18 @@
+var select;
+var msContainer;
+
+beforeEach(function() {
+  $('<select id="multi-select" multiple="multiple" name="test[]"></select>').appendTo('body');
+  for (var i=1; i <= 10; i++) {
+    $('<option value="value'+i+'">text'+i+'</option>').appendTo($("#multi-select"));
+  };
+  select = $("#multi-select");
+});
+
+afterEach(function () {
+  $("#multi-select, #multi-select-optgroup, .ms-container").remove();
+});
+
+sanitize = function(value){
+  return(value.replace(/[^A-Za-z0-9]*/gi, '_'));
+}
