0882960a00ea2e3924306001af64f4bac8998cce
= <pavel.da@core-tech.ru>
2013-02-15 10:36:54 +0000

    Deleted file other.js

--- sites/all/themes/fusion/fusion_theory/js/other.js
+++
@@ -1,7 +0,0 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-   /* $('div#noty_topCenter_layout_container').css({
-                    left: ($(window).width() - $('div#noty_topCenter_layout_container').outerWidth()) / 2 + 'px'
-                });*/
