d48bc2a6761bd6a7ecdd06f7e3b09a4cd18cc3eb
ruslanmp <ruslan.mp@core-tech.ru>
2013-09-16 23:14:17 +0000

    ToW MongoDB node.js server connection script

---
+++ sites/all/modules/_custom/tow_mongodb/js/tow_mongodb.innser_search.js
@@ -0,0 +1,31 @@
+Drupal.behaviors.inner_search = function(context) {
+
+    var port = Drupal.settings.tow_mongodb.hodejs_port;
+    var dataset_nid = Drupal.settings.tow_mongodb.dataset_nid;
+
+    function init() {
+
+        $.ajax({
+            url: 'http://' + location.host + ':' + 1338 + '/dataset',
+            type: 'GET',
+            data: {
+                dataset_nid: dataset_nid
+            },
+            dataType: 'jsonp',
+            async: false,
+            jsonpCallback: 'parseJsonp',
+            success: function(data) {
+                console.log(data);
+            },
+            error: function(jqXHR) {
+                console.log(jqXHR);
+            }
+        });
+    }
+
+    function parseJsonp(json) {
+        return json;
+    }
+
+    init();
+};
